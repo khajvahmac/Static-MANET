2a7974d0c291600899e9f9f5e93016d2896d6c24
khajvah <vahanyan@protonmail.ch>
2017-04-05 22:05:11 +0400

    initial simulation objects

---
+++ src/staticmanet/Coordinate.java
@@ -0,0 +1,30 @@
+package staticmanet;
+
+/**
+ * Created by kv on 4/5/17.
+ */
+public class Coordinate {
+    private float x;
+    private float y;
+
+    public Coordinate(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+}
