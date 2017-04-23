f361acaf45dfd7d6d6184b5443c39bb957b7d4c4
Vahagn Gevorgyan <vahagng@wolfram.com>
2017-04-23 18:33:40 +0400

    no message

--- static-manet/src/staticmanet/HexagonAreaManager.java
+++ static-manet/src/staticmanet/HexagonAreaManager.java
@@ -1,5 +1,6 @@
 package staticmanet;
 
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,15 +8,45 @@ import java.util.List;
  * Created by khajvah on 4/8/17.
  */
 public class HexagonAreaManager {
-    public HexagonAreaManager() {
+    public HexagonAreaManager()
+    {
+        this.tRadius = 10;
+        this.R = tRadius / Math.abs(13);
 
+        for(double i = -X; i <= 2 * X; i += 3 * R) {
+			for(double j = -Y; j <= 2 * Y; j += R * Math.sqrt(3)) {
+				arr.add(new Point(i, j));
+			}
+		}
+		for(double i = -X + R * 3 / 2; i <= 2 * X; i += 3 * R) {
+			for(double j = -Y + R * Math.sqrt(3) / 2; j <= 2 * Y; j += R * Math.sqrt(3)) {
+				arr.add(new Point(i, j));
+			}
+		}
+		
+		Collections.sort(arr, new Comparator<Point>() {
+			public int compare(Point o1, Point o2) {
+				if(o1.y < o2.y || (o1.y == o2.y && o1.x < o2.x))
+                return -1;
+		        return 1;
+		    }
+		});
     }
 
     /**
      * Get the hexagon id from coordinates.
      * */
-    public int getHexagonId(Coordinate coordinate) {
-        return 0;
+    public int getHexagonId(Coordinate coordinate)
+    {
+        double dist = 100000000;
+        int bestHexagonID = -1;
+
+        for(int i = 0; i < arr.size(); i++)
+        if(coordinate.distanceTo(arr.get(i)) <= dist) {
+            dist = coordinate.distanceTo(arr.get(i));
+            bestHexagonID = i;
+        }
+        return bestHexagonID;
     }
 
     /**
@@ -33,4 +64,12 @@ public class HexagonAreaManager {
     public int getClosestTo(int destination, List<Integer> sources) {
         return 0;
     }
+
+    private int X;
+    private int Y;
+    private static double R;
+    private static double tRadius;
+
+    public static List<Coordinate> arr = new ArrayList<Coordinate>();
+
 }
